e9c26c16224e10fe37ef4b994f4af877ac8d12ce
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-07-02 11:44:00 +0530

    [Chore #1]: Removed unused code

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,24 +4,28 @@ import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="card sidebar">
-      <Nav link="/" linkName="Feed" icon={<i className="fas fa-home"></i>} />
-      <Nav
-        link="/explore"
-        linkName="Explore"
-        icon={<i className="fas fa-compass"></i>}
-      />
-      <Nav
-        link="/bookmark"
-        linkName="Bookmark"
-        icon={<i className="fas fa-bookmark"></i>}
-      />
-      <Nav
-        link="/profile"
-        linkName="Profile"
-        icon={<i class="fas fa-user"></i>}
-      />
-      <button className="btn btn-primary btn-create">Create new post</button>
+    <div className="sidebar-container">
+      <div className="card sidebar">
+        <Nav link="/" linkName="Feed" icon={<i className="fas fa-home"></i>} />
+        <Nav
+          link="/explore"
+          linkName="Explore"
+          icon={<i className="fas fa-compass"></i>}
+        />
+        <Nav
+          link="/bookmark"
+          linkName="Bookmark"
+          icon={<i className="fas fa-bookmark"></i>}
+        />
+        <Nav
+          link="/profile"
+          linkName="Profile"
+          icon={<i class="fas fa-user"></i>}
+        />
+      </div>
+      <div className="new-post-btn my-2">
+        <button className="btn btn-primary btn-create">Create new post</button>
+      </div>
     </div>
   );
 }
